dd8e0dc5c4c8c572b6c03a6cc156646797cad5d4
kiran_kumar <139195365+KiranKumar4024@users.noreply.github.com>
2023-07-24 11:01:50 +0530

    Create PosNeg.java

---
+++ Day1/PosNeg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PosNeg {
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a==0)
+{
+System.out.println("Zero");
+}
+if(a>0){
+System.out.println("positive");
+}
+else{
+System.out.println("Negative");
+}
+}
+}
